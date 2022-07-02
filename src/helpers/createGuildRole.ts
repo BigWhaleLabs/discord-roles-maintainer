04b175c6082848a4cfc35f66e69ee1b6f72fb1f5
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-07-02 07:55:49 -0700

    fix roles

--- src/helpers/createGuildRole.ts
+++ src/helpers/createGuildRole.ts
@@ -1,4 +1,4 @@
-import { role } from '@guildxyz/sdk'
+import { Requirement, role } from '@guildxyz/sdk'
 import env from '@/helpers/env'
 import signer from '@/helpers/signer'
 import wallet from '@/helpers/wallet'
@@ -19,25 +19,41 @@ export default async function (name: string, address: string) {
         chain: 'GOERLI',
         address,
         data: {
-          amount: 1,
-        },
+          minAmount: 1,
+        } as unknown as { amount: number },
       },
     ],
   })
   // Update verified holders
   const verifiedHolderRole = await role.get(env.VERIFIED_HOLDER_ROLE_ID)
   const requirements = verifiedHolderRole.requirements
-  requirements.push({
-    type: 'ERC721',
-    chain: 'GOERLI',
-    address,
-    data: {
-      amount: 1,
-    },
-  })
-  await role.update(env.VERIFIED_HOLDER_ROLE_ID, wallet.address, signer, {
-    name,
-    logic: 'OR',
-    requirements,
-  })
+    .map((r) => {
+      if (r.type !== 'ERC721') {
+        return
+      }
+      return {
+        type: r.type,
+        chain: r.chain,
+        address: r.address,
+        data: {
+          minAmount: 1,
+        } as unknown as { amount: number },
+      }
+    })
+    .filter((r) => !!r) as Requirement[]
+  if (!requirements.find((r) => 'address' in r && r.address === address)) {
+    requirements.push({
+      type: 'ERC721',
+      chain: 'GOERLI',
+      address,
+      data: {
+        minAmount: 1,
+      } as unknown as { amount: number },
+    })
+    await role.update(env.VERIFIED_HOLDER_ROLE_ID, wallet.address, signer, {
+      name: verifiedHolderRole.name,
+      logic: 'OR',
+      requirements,
+    })
+  }
 }
